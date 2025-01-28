c0b799b47e8933e338679c435e838002ea7aee80
Ivan Espinosa <isespinosa@student.hau.edu.ph>
2025-01-28 15:47:03 +0800

    Quiz3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./screen/Main";
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './screens/Dashboard';
+import ProductScreen from './screens/ProductScreen';
+import UserScreen from './screens/UserScreen';
 
-function App() {
+const App = () => {
   return (
-    <div className="app-container">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/product/:uuid" element={<ProductScreen />} />
+          <Route path="/user/:id" element={<UserScreen />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
